faf135591098e679eb24227e87f0bfc0c9778333
syaloni06 <syalonib@gmail.com>
2025-02-05 00:18:13 +0530

    dashboard integrated

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,16 @@
+import Chart from "./TextEditor/Chart";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { saveUserInfo } from "../utils/userSlice"; // Redux action
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const storedUserData = JSON.parse(localStorage.getItem("userData")) || [];
+    dispatch(saveUserInfo(storedUserData))
+  }, [])
+  
     return (
-      <div>Dashboard</div>
+      <div><Chart/></div>
     )
   }
   
